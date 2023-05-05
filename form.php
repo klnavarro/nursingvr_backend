e5af42da00ec68bc6972552471e8fe1987fe8523
kathuluuu <kthlnnvrr@gmail.com>
2023-05-04 17:38:41 -0700

    adding form

---
+++ form.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+  <form action ="models/S1.php" method=post>
+
+    <label for="Ant_RUL">Ant_RUL</label>
+    <input type="text" id="Ant_RUL" name = "Ant_RUL"> 
+    <br><br>
+    <label for="Ant_RML">Ant_RML</label>
+    <input type="text" id="Ant_RML" name = "Ant_RML"> 
+    <br><br>
+    <label for="Ant_RLL">Ant_RLL</label>
+    <input type="text" id="Ant_RLL" name = "Ant_RLL"> 
+    <br><br>
+    <label for="Ant_RUL2">Ant_LUL</label>
+    <input type="text" id="Ant_RUL2" name = "Ant_RUL2"> 
+    <br><br>
+    <label for="Ant_LLL">Ant_LLL</label>
+    <input type="text" id="Ant_LLL" name = "Ant_LLL"> 
+    <br><br>
+    <label for="Post_RUL">POST_RUL</label>
+    <input type="text" id="Post_RUL" name = "Post_RUL"> 
+    <br><br>
+    <label for="Post_RLL">POST_RLL</label>
+    <input type="text" id="Post_RLL" name = "Post_RLL"> 
+    <br><br>
+    <label for="Post_RUL2">POST_LUL</label>
+    <input type="text" id="Post_LUL" name = "Post_LUL"> 
+    <br><br>
+    <label for="Post_LLL">POST_LLL</label>
+    <input type="text" id="Post_LLL" name = "Post_LLL"> 
+    <br><br>
+    <label for="Lat_RUL">Lat_RUL</label>
+    <input type="text" id="Lat_RUL" name = "Lat_RUL"> 
+    <br><br>
+    <label for="Lat_RML">Lat_RML</label>
+    <input type="text" id="Lat_RML" name = "Lat_RML"> 
+    <br><br>
+    <label for="Lat_RLL">Lat_RLL</label>
+    <input type="text" id="Lat_RLL" name = "Lat_RLL"> 
+    <br><br>
+    <label for="Lat_LUL">Lat_LUL</label>
+    <input type="text" id="Lat_LUL" name = "Lat_LUL"> 
+    <br><br>
+    <label for="Lat_LLL">Lat_LLL</label>
+    <input type="text" id="Lat_LLL" name = "Lat_LLL"> 
+    <br><br>
+    <label for="leftUnmeasured">leftUnmeasured</label>
+    <input type="text" id="leftUnmeasured" name = "leftUnmeasured"> 
+    <br><br>
+
+    <button>send</button>
+</form>
+
+</html>
